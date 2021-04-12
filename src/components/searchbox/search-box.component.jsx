5c60836c7bc8c716a52d363a3cce6f99960b17da
sumedh <sumedh.upgade@kouux.com>
2021-04-12 19:07:50 +0530

    Updates

--- src/components/searchbox/search-box.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Searchbox = ({placeholder, handelChange}) => (
-    <input type="search" 
-            className="mt10 mb20"
-            placeholder={placeholder}
-            onChange={handelChange}>
-          </input>
-)
